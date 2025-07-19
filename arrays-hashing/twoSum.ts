6723939ca032a94f7ab399dba98e644dad655cfe
mihrilp <mihrilp@gmail.com>
2025-07-19 16:02:29 +0300

    Add twoSum algorithm with hashmap

---
+++ arrays-hashing/twoSum.ts
@@ -0,0 +1,31 @@
+/*
+🧠 Problem: Two Sum
+📦 Topic: Arrays & Hashing
+📊 Difficulty: Easy
+🛠️ Approach: HashMap
+⏱️ Time: O(n)
+📦 Space: O(n)
+
+📝 Description:
+Given an array of integers and a target, return indices of the two numbers such that they add up to the target.
+
+💡 Approach:
+- Iterate over nums, for each number compute complement = target - num.
+- Check if complement exists in map.
+- If yes, return indices.
+- Else, store current number and index in map.
+*/
+
+function twoSum(nums: number[], target: number): number[] {
+    const map = new Map<number, number>();
+
+    for (const [index, num] of nums.entries()) {
+        const complement = target - num;
+        if (map.has(complement)) {
+            return [map.get(complement)!, index]
+        }
+        map.set(num, index)
+    }
+
+    return [];
+}
