d5b28a359cb05cda669ee07dc1e9da3c8b91024c
mihrilp <mihrilp@gmail.com>
2025-07-29 14:32:16 +0300

    app containsDuplicate function using HashSet

---
+++ arrays-hashing/containsDuplicate.ts
@@ -0,0 +1,29 @@
+/*
+🧠 Problem: Contains Duplicate  
+📦 Topic: Arrays & Hashing  
+📊 Difficulty: Easy  
+🛠️ Approach: HashSet  
+⏱️ Time: O(n)  
+📦 Space: O(n)  
+
+📝 Description:  
+Given an integer array `nums`, return `true` if any value appears at least twice in the array, and `false` if every element is distinct.
+
+💡 Approach:  
+- Initialize an empty Set to store seen numbers.  
+- Iterate over `nums`:  
+  - If the current number already exists in the Set, return true.  
+  - Otherwise, add the number to the Set.  
+- If the loop completes without finding duplicates, return false.
+*/
+
+function containsDuplicate(nums: number[]): boolean {
+    const set = new Set<number>();
+
+    for (const num of nums) {
+        if (set.has(num)) return true;
+        set.add(num);
+    }
+    
+    return false;
+}
